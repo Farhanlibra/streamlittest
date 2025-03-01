86669bfd6d6b7d9ad0660f4d7f10916bbcf37f4e
Farhanlibra <137736801+Farhanlibra@users.noreply.github.com>
2025-02-28 22:30:56 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 
 
 import streamlit as st
-from pypdf import PdfReader
+
 
 
 
@@ -12,13 +12,9 @@ def main():
     st.header("ask your pdf")
     st.button("test")
 
-    pdf= st.file_uploader("upload your PDF", type="pdf")
+    
 
-    if pdf is not None:
-        pdf_reader = PdfReader(pdf)
-        text=""
-        for page in pdf_reader.pages:
-            text += page.extract_text()
+   
     
 
     if __name__ == '__main__':
